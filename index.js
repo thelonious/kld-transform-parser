0d498dd2d1de4d1ad85d7baa27a700224715d556
Kevin Lindsey <kevin@kevlindev.com>
2019-05-05 15:13:21 -0700

    Use JSDoc modules
    This seems to have improved the docs

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+/**
+ *  @module kld-transform-parser
+ */
+
 // expose classes
 
+/**
+ * @namespace TransformLexer
+ * @implements {module:TransformLexer~TransformLexer}
+ */
 export {default as TransformLexer} from "./lib/TransformLexer.js";
+
+/**
+ * @namespace TransformParser
+ * @implements {module:TransformParser~TransformParser}
+ */
 export {default as TransformParser} from "./lib/TransformParser.js";
+
+/**
+ * @namespace SampleHandler
+ * @implements {module:SampleHandler~SampleHandler}
+ */
 export {default as SampleHandler} from "./lib/SampleHandler.js";
